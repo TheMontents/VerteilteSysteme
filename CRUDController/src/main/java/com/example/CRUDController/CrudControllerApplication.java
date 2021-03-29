6255045620e14353367e96908cf398eae477fb89
Marcel Autenrieth <maauit02@hs-esslingen.de>
2021-03-29 15:24:01 +0200

    added new CRUDController Project

---
+++ CRUDController/src/main/java/com/example/CRUDController/CrudControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudControllerApplication.class, args);
+	}
+
+}
