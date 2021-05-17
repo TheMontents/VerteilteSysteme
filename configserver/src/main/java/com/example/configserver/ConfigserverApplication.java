a5902d3cb53e051fd6052e0f9f6b685665b0866e
Marcel Autenrieth <maauit02@hs-esslingen.de>
2021-05-17 15:55:52 +0200

    bisschen was mit propperties

--- configserver/src/main/java/com/example/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/example/configserver/ConfigserverApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConfigserverApplication {
-
+public class ConfigserverApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
