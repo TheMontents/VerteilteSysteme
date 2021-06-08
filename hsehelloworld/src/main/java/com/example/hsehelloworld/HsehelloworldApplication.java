22e0eafba45d5240d725def7bbba40ff42df2982
Marcel Autenrieth <maauit02@hs-esslingen.de>
2021-06-08 13:29:08 +0200

    added external config server and played around with first application

--- hsehelloworld/src/main/java/com/example/hsehelloworld/HsehelloworldApplication.java
+++ hsehelloworld/src/main/java/com/example/hsehelloworld/HsehelloworldApplication.java
@@ -1,7 +1,9 @@
 package com.example.hsehelloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class HsehelloworldApplication {
 
 	private String state = "no_state";
+
+	@Value("${config.source:unknown}")
+	private String configVariable;
+
+	@GetMapping("/config")
+	public String getConfigSource(){
+		return "Configuration Source: " + configVariable;
+	}
 
 	@GetMapping
 	public String helloMarcel(){
